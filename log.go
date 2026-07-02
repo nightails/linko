b53d5fdc4fc4bed2b9d03acd14d51fea4eb75737
Alan Night <a.nightails@gmail.com>
2026-07-02 14:30:53 -0500

    Refactor logger initialization to support multiple handlers, improve error logging, and streamline cleanup routines

--- log.go
+++ log.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"log/slog"
 	"net/http"
 	"os"
@@ -27,34 +26,82 @@ type LogContext struct {
 type closeFunc func() error
 
 func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
+	var (
+		handlers []slog.Handler
+		closers  []closeFunc
+	)
+
+	replaceAttr := func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == "error" {
+			err, ok := a.Value.Any().(error)
+			if !ok {
+				return a
+			}
+
+			if multiErr, ok := errors.AsType[multiError](err); ok {
+				errorAttrs := make([]slog.Attr, 0, len(multiErr.Unwrap()))
+
+				for i, childErr := range multiErr.Unwrap() {
+					childAttrs := []slog.Attr{
+						{
+							Key:   "message",
+							Value: slog.StringValue(childErr.Error()),
+						},
+					}
+
+					childAttrs = append(childAttrs, linkoerr.Attrs(childErr)...)
+
+					errorAttrs = append(errorAttrs, slog.GroupAttrs(
+						fmt.Sprintf("error_%d", i+1),
+						childAttrs...,
+					))
+				}
+
+				return slog.GroupAttrs("errors", errorAttrs...)
+			}
+
+			attrs := []slog.Attr{
+				{
+					Key:   "message",
+					Value: slog.StringValue(err.Error()),
+				},
+			}
+
+			if stackErr, ok := errors.AsType[stackTracer](err); ok {
+				attrs = append(attrs, slog.Attr{
+					Key:   "stack_trace",
+					Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+				})
+			}
+
+			attrs = append(attrs, linkoerr.Attrs(err)...)
+
+			return slog.GroupAttrs("error", attrs...)
+		}
+		return a
+	}
+
+	// console logger
 	w := os.Stderr
-	// default debug handler with tint
 	debugHandler := tint.NewHandler(w, &tint.Options{
 		Level:       slog.LevelDebug,
 		ReplaceAttr: replaceAttr,
-		NoColor:     !isatty.IsTerminal(w.Fd()) && !isatty.IsCygwinTerminal(w.Fd()),
+		NoColor:     !isatty.IsTerminal(w.Fd()) || isatty.IsCygwinTerminal(w.Fd()),
 	})
-
-	closeLogger := func() error {
-		return nil
-	}
-	logger := slog.New(debugHandler)
+	handlers = append(handlers, debugHandler)
 
 	// open log file
 	if logFile != "" {
-		file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+		file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0x644)
 		if err != nil {
 			return nil, nil, fmt.Errorf("failed to open log file: %w", err)
 		}
-		bufferedFile := bufio.NewWriterSize(file, 8192)
-		multiWriter := io.MultiWriter(os.Stderr, bufferedFile)
-
-		infoHandler := slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{
+		bufferedFile := bufio.NewWriter(file)
+		handlers = append(handlers, slog.NewJSONHandler(bufferedFile, &slog.HandlerOptions{
 			Level:       slog.LevelInfo,
 			ReplaceAttr: replaceAttr,
-		})
-
-		closeLogger = func() error {
+		}))
+		closers = append(closers, func() error {
 			if err := bufferedFile.Flush(); err != nil {
 				return fmt.Errorf("failed to flush buffered log file: %w", err)
 			}
@@ -62,10 +109,28 @@ func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
 				return fmt.Errorf("failed to close log file: %w", err)
 			}
 			return nil
-		}
+		})
 
-		logger = slog.New(slog.NewMultiHandler(debugHandler, infoHandler))
+		closers = append(closers, func() error {
+			if err := bufferedFile.Flush(); err != nil {
+				return fmt.Errorf("failed to flush buffered log file: %w", err)
+			}
+			if err := file.Close(); err != nil {
+				return fmt.Errorf("failed to close log file: %w", err)
+			}
+			return nil
+		})
 	}
+
+	cleanup := func() error {
+		var errs []error
+		for _, closer := range closers {
+			errs = append(errs, closer())
+		}
+		return errors.Join(errs...)
+	}
+
+	logger := slog.New(slog.NewMultiHandler(handlers...))
 
 	env := os.Getenv("ENV")
 	hostname, _ := os.Hostname()
@@ -78,7 +143,7 @@ func initializeLogger(logFile string) (*slog.Logger, closeFunc, error) {
 		slog.String("hostname", hostname),
 	)
 
-	return logger, closeLogger, nil
+	return logger, cleanup, nil
 }
 
 type stackTracer interface {
@@ -89,56 +154,6 @@ type stackTracer interface {
 type multiError interface {
 	error
 	Unwrap() []error
-}
-
-func replaceAttr(groups []string, a slog.Attr) slog.Attr {
-	if a.Key == "error" {
-		err, ok := a.Value.Any().(error)
-		if !ok {
-			return a
-		}
-
-		if multiErr, ok := errors.AsType[multiError](err); ok {
-			errorAttrs := make([]slog.Attr, 0, len(multiErr.Unwrap()))
-
-			for i, childErr := range multiErr.Unwrap() {
-				childAttrs := []slog.Attr{
-					{
-						Key:   "message",
-						Value: slog.StringValue(childErr.Error()),
-					},
-				}
-
-				childAttrs = append(childAttrs, linkoerr.Attrs(childErr)...)
-
-				errorAttrs = append(errorAttrs, slog.GroupAttrs(
-					fmt.Sprintf("error_%d", i+1),
-					childAttrs...,
-				))
-			}
-
-			return slog.GroupAttrs("errors", errorAttrs...)
-		}
-
-		attrs := []slog.Attr{
-			{
-				Key:   "message",
-				Value: slog.StringValue(err.Error()),
-			},
-		}
-
-		if stackErr, ok := errors.AsType[stackTracer](err); ok {
-			attrs = append(attrs, slog.Attr{
-				Key:   "stack_trace",
-				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
-			})
-		}
-
-		attrs = append(attrs, linkoerr.Attrs(err)...)
-
-		return slog.GroupAttrs("error", attrs...)
-	}
-	return a
 }
 
 func httpError(ctx context.Context, w http.ResponseWriter, status int, err error) {
