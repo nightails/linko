502326b88bddc69a1babef0d92c908ce48ee852a
Alan Night <a.nightails@gmail.com>
2026-07-21 18:33:00 -0500

    Add HTTP request metrics middleware with Prometheus integration

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
-		Handler: requestID(requestLogger(logger)(mux)),
+		Handler: metricsMiddleware(requestID(requestLogger(logger)(mux))),
 	}
 
 	s := &server{
